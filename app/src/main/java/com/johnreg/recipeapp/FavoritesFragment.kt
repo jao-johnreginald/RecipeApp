a0201c239f1990b5e2ad9ca37a2fdb9b69f9b253
John Reg <johnreginaldjao@gmail.com>
2024-09-10 17:24:32 +0800

    Navigation Component Setup

---
+++ app/src/main/java/com/johnreg/recipeapp/FavoritesFragment.kt
@@ -0,0 +1,25 @@
+package com.johnreg.recipeapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.johnreg.recipeapp.databinding.FragmentFavoritesBinding
+
+class FavoritesFragment : Fragment() {
+
+    private lateinit var binding: FragmentFavoritesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
