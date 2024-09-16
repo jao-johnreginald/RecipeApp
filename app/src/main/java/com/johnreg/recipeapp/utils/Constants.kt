ac0429c6564f0dd5f9e7d6bbf2b5060ccd2e991d
John Reg <johnreginaldjao@gmail.com>
2024-09-16 17:16:01 +0800

    Create Constants class

---
+++ app/src/main/java/com/johnreg/recipeapp/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.johnreg.recipeapp.utils
+
+object Constants {
+
+    const val API_KEY = "28dabea8508a41f9bff21533bf8e6034"
+
+    const val BASE_URL = "https://api.spoonacular.com"
+
+}
