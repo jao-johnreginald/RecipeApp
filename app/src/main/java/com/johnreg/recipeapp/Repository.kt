0211675f35316efd74ff2b6932640b705465e8e7
John Reg <johnreginaldjao@gmail.com>
2024-09-17 20:51:51 +0800

    Create and Inject Repository class

---
+++ app/src/main/java/com/johnreg/recipeapp/Repository.kt
@@ -0,0 +1,19 @@
+package com.johnreg.recipeapp
+
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import javax.inject.Inject
+
+/*
+@ActivityRetainedScoped - a scope annotation for bindings that should exist
+for the life of an activity, and survive a configuration change as well
+
+Since we're going to inject this Repository in a ViewModel later, I have just added this
+@ActivityRetainedScoped so this Repository can survive a configuration change as well
+and we're going to have the same instance even when a user is rotating the screen
+ */
+@ActivityRetainedScoped
+class Repository @Inject constructor(
+    remoteDataSource: RemoteDataSource
+) {
+    val remote = remoteDataSource
+}
