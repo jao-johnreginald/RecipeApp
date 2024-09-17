d71c1fb27724df2edea847d8bbb7b3f79d0fcbd2
John Reg <johnreginaldjao@gmail.com>
2024-09-17 12:52:08 +0800

    Create Application class

---
+++ app/src/main/java/com/johnreg/recipeapp/RecipeApp.kt
@@ -0,0 +1,8 @@
+package com.johnreg.recipeapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+// This is the place where the dagger components should be generated automatically for us
+@HiltAndroidApp
+class RecipeApp : Application()
