a652c5300b227e1570bae0699fdefb730db16cd6
John Reg <johnreginaldjao@gmail.com>
2024-09-17 12:41:35 +0800

    Create RecipeApi interface

---
+++ app/src/main/java/com/johnreg/recipeapp/RecipeApi.kt
@@ -0,0 +1,21 @@
+package com.johnreg.recipeapp
+
+import com.johnreg.recipeapp.models.Recipe
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+interface RecipeApi {
+
+    /*
+    @GET - here we have specified the exact endpoint to request and receive data from our api
+    suspend - this function will run on a background thread instead of the main thread
+    @QueryMap - lets us create something like a HashMap to add all of our queries at once
+    Response - a simple class for an http response
+     */
+    @GET("/recipes/complexSearch")
+    suspend fun getRecipe(
+        @QueryMap queries: Map<String, String>
+    ): Response<Recipe>
+
+}
