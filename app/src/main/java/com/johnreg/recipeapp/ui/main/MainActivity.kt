b4f2b2c159c702ce4b8587fa4449b500b96e04d8
John Reg <johnreginaldjao@gmail.com>
2024-09-19 22:27:16 +0800

    added @AndroidEntryPoint in MainActivity

--- app/src/main/java/com/johnreg/recipeapp/ui/main/MainActivity.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/main/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.johnreg.recipeapp.R
 import com.johnreg.recipeapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
