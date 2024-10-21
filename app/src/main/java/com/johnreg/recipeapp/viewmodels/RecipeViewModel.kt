1f1359c0e389768c271292150beb1bda1917e6ed
John Reg <johnreginaldjao@gmail.com>
2024-10-22 01:52:28 +0800

    Modify and Inject RecipeViewModel

--- app/src/main/java/com/johnreg/recipeapp/viewmodels/RecipeViewModel.kt
+++ app/src/main/java/com/johnreg/recipeapp/viewmodels/RecipeViewModel.kt
@@ -2,6 +2,8 @@ package com.johnreg.recipeapp.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.johnreg.recipeapp.data.repositories.DataStoreRepository
 import com.johnreg.recipeapp.utils.Constants.API_KEY
 import com.johnreg.recipeapp.utils.Constants.DEFAULT_DIET_TYPE
 import com.johnreg.recipeapp.utils.Constants.DEFAULT_MEAL_TYPE
@@ -13,20 +15,46 @@ import com.johnreg.recipeapp.utils.Constants.QUERY_FILL_INGREDIENTS
 import com.johnreg.recipeapp.utils.Constants.QUERY_NUMBER
 import com.johnreg.recipeapp.utils.Constants.QUERY_TYPE
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class RecipeViewModel @Inject constructor(
+    private val dataStoreRepository: DataStoreRepository,
     application: Application
 ) : AndroidViewModel(application) {
 
-    fun getQueryMap(): HashMap<String, String> = hashMapOf(
-        QUERY_NUMBER to DEFAULT_RESULT_COUNT,
-        QUERY_API_KEY to API_KEY,
-        QUERY_TYPE to DEFAULT_MEAL_TYPE,
-        QUERY_DIET to DEFAULT_DIET_TYPE,
-        QUERY_ADD_RECIPE_INFORMATION to "true",
-        QUERY_FILL_INGREDIENTS to "true"
-    )
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val types = dataStoreRepository.getTypesPreferences()
+
+    fun saveMealAndDietType(
+        mealTypeName: String, mealTypeId: Int, dietTypeName: String, dietTypeId: Int
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        dataStoreRepository.setTypesPreferences(mealTypeName, mealTypeId, dietTypeName, dietTypeId)
+    }
+
+    fun getQueryMap(): HashMap<String, String> {
+        viewModelScope.launch {
+            // Collect the values from that Flow and store them inside those variables
+            types.collect { types ->
+                mealType = types.mealTypeName
+                dietType = types.dietTypeName
+            }
+        }
+
+        // Get the newest values inside our getQueryMap() every time
+        // If there is no data inside those types, get their default values
+        return hashMapOf(
+            QUERY_NUMBER to DEFAULT_RESULT_COUNT,
+            QUERY_API_KEY to API_KEY,
+            QUERY_TYPE to mealType,
+            QUERY_DIET to dietType,
+            QUERY_ADD_RECIPE_INFORMATION to "true",
+            QUERY_FILL_INGREDIENTS to "true"
+        )
+    }
 
 }
