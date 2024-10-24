1bcaa354f1c9564a20d05407a44aabeaba6e12b3
John Reg <johnreginaldjao@gmail.com>
2024-10-24 20:15:17 +0800

    Create NetworkCallback class

---
+++ app/src/main/java/com/johnreg/recipeapp/utils/NetworkCallback.kt
@@ -0,0 +1,47 @@
+package com.johnreg.recipeapp.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class NetworkCallback : ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable: MutableStateFlow<Boolean> = MutableStateFlow(false)
+
+    override fun onAvailable(network: Network) {
+        super.onAvailable(network)
+        isNetworkAvailable.value = true
+    }
+
+    override fun onLost(network: Network) {
+        super.onLost(network)
+        isNetworkAvailable.value = false
+    }
+
+    fun isNetworkAvailable(context: Context): StateFlow<Boolean> {
+        val connectivityManager = context.getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        connectivityManager.registerDefaultNetworkCallback(this)
+        val network = connectivityManager.activeNetwork
+        val capabilities = connectivityManager.getNetworkCapabilities(network)
+
+        isNetworkAvailable.value = if (network != null && capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            false
+        }
+
+        return isNetworkAvailable
+    }
+
+}
