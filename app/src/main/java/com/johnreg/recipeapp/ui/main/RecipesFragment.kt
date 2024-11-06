95b3b2f0f1ad973c8d7e7ed2212baa2974ebf8af
John Reg <johnreginaldjao@gmail.com>
2024-11-06 18:50:11 +0800

    changed SearchView import

--- app/src/main/java/com/johnreg/recipeapp/ui/main/RecipesFragment.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/main/RecipesFragment.kt
@@ -12,7 +12,7 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
