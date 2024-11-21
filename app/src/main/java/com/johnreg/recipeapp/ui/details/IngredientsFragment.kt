d5530c46c27e9516cc648b86e523805f2e87e297
John Reg <johnreginaldjao@gmail.com>
2024-11-22 01:35:06 +0800

    IngredientsFragment Display Ingredients Data

--- app/src/main/java/com/johnreg/recipeapp/ui/details/IngredientsFragment.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/details/IngredientsFragment.kt
@@ -5,11 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.johnreg.recipeapp.databinding.FragmentIngredientsBinding
+import com.johnreg.recipeapp.models.Result
+import com.johnreg.recipeapp.ui.adapters.IngredientsAdapter
+import com.johnreg.recipeapp.utils.Constants.RESULT_BUNDLE_KEY
+import com.johnreg.recipeapp.utils.getParcelableExtra
 
 class IngredientsFragment : Fragment() {
 
     private lateinit var binding: FragmentIngredientsBinding
+
+    private val ingredientsAdapter by lazy { IngredientsAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -20,6 +27,24 @@ class IngredientsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setRecyclerView()
+        getResultBundleAndSetUI()
+    }
+
+    private fun setRecyclerView() {
+        binding.rvIngredients.apply {
+            adapter = ingredientsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun getResultBundleAndSetUI() {
+        val resultBundle = requireArguments().getParcelableExtra(RESULT_BUNDLE_KEY) as? Result
+
+        resultBundle?.let { result ->
+            ingredientsAdapter.setIngredients(result.ingredients)
+        }
     }
 
 }
