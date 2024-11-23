dbaabb98356562d0aab76023ad45b66ae8f7712c
John Reg <johnreginaldjao@gmail.com>
2024-11-23 23:22:01 +0800

    Modify RecipeDao interface

--- app/src/main/java/com/johnreg/recipeapp/data/local/RecipeDao.kt
+++ app/src/main/java/com/johnreg/recipeapp/data/local/RecipeDao.kt
@@ -1,9 +1,11 @@
 package com.johnreg.recipeapp.data.local
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.johnreg.recipeapp.data.entities.FavoriteEntity
 import com.johnreg.recipeapp.data.entities.RecipeEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -13,9 +15,21 @@ interface RecipeDao {
     @Query("SELECT * FROM recipe_table ORDER BY id ASC")
     fun getRecipes(): Flow<List<RecipeEntity>>
 
+    @Query("SELECT * FROM favorites_table ORDER BY id ASC")
+    fun getFavorites(): Flow<List<FavoriteEntity>>
+
     // REPLACE - whenever we fetch a new data from our api, we want to replace the old one
     // Our table will have the newest recipes everytime we request a new data from our api
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertRecipe(recipeEntity: RecipeEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favoriteEntity: FavoriteEntity)
+
+    @Delete
+    suspend fun deleteFavorite(favoriteEntity: FavoriteEntity)
+
+    @Query("DELETE FROM favorites_table")
+    suspend fun deleteAllFavorites()
 
 }
