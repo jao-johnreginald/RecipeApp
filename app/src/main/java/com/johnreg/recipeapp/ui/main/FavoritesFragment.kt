8b2aa43d67ed223f444deabd7dd592cc398eb675
John Reg <johnreginaldjao@gmail.com>
2024-11-30 01:15:03 +0800

    Display Favorite Recipes

--- app/src/main/java/com/johnreg/recipeapp/ui/main/FavoritesFragment.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/main/FavoritesFragment.kt
@@ -5,11 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.johnreg.recipeapp.databinding.FragmentFavoritesBinding
+import com.johnreg.recipeapp.ui.adapters.FavoritesAdapter
+import com.johnreg.recipeapp.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
 
     private lateinit var binding: FragmentFavoritesBinding
+
+    private val favoritesAdapter by lazy { FavoritesAdapter() }
+
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -20,6 +30,30 @@ class FavoritesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setRecyclerView()
+        observeFavorites()
+    }
+
+    private fun setRecyclerView() {
+        binding.rvFavorites.apply {
+            adapter = favoritesAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun observeFavorites() {
+        mainViewModel.favorites.observe(viewLifecycleOwner) { favoriteEntityList ->
+            if (favoriteEntityList.isEmpty()) {
+                binding.ivNoData.visibility = View.VISIBLE
+                binding.tvNoData.visibility = View.VISIBLE
+            } else {
+                binding.ivNoData.visibility = View.INVISIBLE
+                binding.tvNoData.visibility = View.INVISIBLE
+            }
+
+            favoritesAdapter.setFavorites(favoriteEntityList)
+        }
     }
 
 }
