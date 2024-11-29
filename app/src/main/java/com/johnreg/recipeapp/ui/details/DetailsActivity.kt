b877f0a17d35ae5e20b75224dd8acc58abe18c19
John Reg <johnreginaldjao@gmail.com>
2024-11-29 11:43:57 +0800

    FAVORITE AND UNFAVORITE RECIPE

--- app/src/main/java/com/johnreg/recipeapp/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/details/DetailsActivity.kt
@@ -1,28 +1,98 @@
 package com.johnreg.recipeapp.ui.details
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.core.view.MenuProvider
 import androidx.navigation.navArgs
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayoutMediator
 import com.johnreg.recipeapp.R
+import com.johnreg.recipeapp.data.entities.FavoriteEntity
 import com.johnreg.recipeapp.databinding.ActivityDetailsBinding
 import com.johnreg.recipeapp.ui.adapters.PagerAdapter
 import com.johnreg.recipeapp.utils.Constants.RESULT_BUNDLE_KEY
+import com.johnreg.recipeapp.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsBinding
 
+    private lateinit var menuItem: MenuItem
+
+    private val mainViewModel: MainViewModel by viewModels()
     private val args: DetailsActivityArgs by navArgs()
+
+    private var favoriteId = 0
+    private var isFavorite = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setMenu()
         setToolbar()
         setViewPager2AndTabLayout()
+    }
+
+    private fun setMenu() {
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.details_menu, menu)
+                menuItem = menu.findItem(R.id.menu_favorite)
+                observeFavorites()
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_favorite -> {
+                        if (isFavorite) {
+                            mainViewModel.deleteFavorite(FavoriteEntity(args.result, favoriteId))
+                            showSnackbar("Removed from Favorites.")
+                        } else {
+                            mainViewModel.insertFavorite(FavoriteEntity(args.result))
+                            showSnackbar("Saved to Favorites.")
+                        }
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        })
+    }
+
+    private fun observeFavorites() {
+        mainViewModel.favorites.observe(this) { favoriteEntityList ->
+            try {
+                for (favoriteEntity in favoriteEntityList) {
+                    if (favoriteEntity.result.id == args.result.id) {
+                        menuItem.icon!!.setTint(getColor(R.color.yellow))
+                        favoriteId = favoriteEntity.id
+                        isFavorite = true
+                        // Exit the observer lambda since a match is found
+                        return@observe
+                    }
+                }
+                // Execute only if no match was found
+                menuItem.icon!!.setTint(getColor(R.color.white))
+                isFavorite = false
+            } catch (e: Exception) {
+                Log.e("DetailsActivity", e.localizedMessage, e)
+            }
+        }
+    }
+
+    private fun showSnackbar(message: String) {
+        Snackbar.make(binding.root, message, Snackbar.LENGTH_SHORT).setAction("Okay") {}.show()
     }
 
     private fun setToolbar() {
