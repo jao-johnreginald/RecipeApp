9dc726ad90adc812fca6e58c80e096bf4cf825c4
John Reg <johnreginaldjao@gmail.com>
2024-12-05 21:51:05 +0800

    modified FavoritesAdapter

--- app/src/main/java/com/johnreg/recipeapp/ui/adapters/FavoritesAdapter.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/adapters/FavoritesAdapter.kt
@@ -1,16 +1,37 @@
 package com.johnreg.recipeapp.ui.adapters
 
+import android.view.ActionMode
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ColorRes
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import com.johnreg.recipeapp.R
 import com.johnreg.recipeapp.data.entities.FavoriteEntity
 import com.johnreg.recipeapp.ui.main.FavoritesFragmentDirections
 import com.johnreg.recipeapp.utils.DiffUtilCallback
+import com.johnreg.recipeapp.utils.setCardColors
+import com.johnreg.recipeapp.viewmodels.MainViewModel
 
-class FavoritesAdapter : RecyclerView.Adapter<RecipesViewHolder>() {
+class FavoritesAdapter(
+    private val fragmentActivity: FragmentActivity,
+    private val mainViewModel: MainViewModel
+) : RecyclerView.Adapter<RecipesViewHolder>(), ActionMode.Callback {
 
     private var favorites: List<FavoriteEntity> = emptyList()
+
+    private lateinit var rootView: View
+    private lateinit var actionMode: ActionMode
+
+    private var viewHolders: ArrayList<RecipesViewHolder> = arrayListOf()
+    private var selectedItems: ArrayList<FavoriteEntity> = arrayListOf()
+
+    private var isActionModeStarted = false
 
     override fun getItemCount(): Int {
         return favorites.size
@@ -21,15 +42,77 @@ class FavoritesAdapter : RecyclerView.Adapter<RecipesViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: RecipesViewHolder, position: Int) {
-        holder.bind(favorites[position].result)
+        rootView = holder.itemView.rootView
+        viewHolders.add(holder)
+
+        val favoriteEntity = favorites[position]
+        holder.bind(favoriteEntity.result)
 
         holder.itemView.setOnClickListener {
-            holder.itemView.findNavController().navigate(
-                FavoritesFragmentDirections.actionFavoritesFragmentToDetailsActivity(
-                    favorites[position].result
+            if (!isActionModeStarted) {
+                holder.itemView.findNavController().navigate(
+                    FavoritesFragmentDirections.actionFavoritesFragmentToDetailsActivity(
+                        favoriteEntity.result
+                    )
                 )
-            )
+            } else {
+                setSelectionAndTitle(holder, favoriteEntity)
+            }
         }
+
+        holder.itemView.setOnLongClickListener {
+            if (!isActionModeStarted) {
+                isActionModeStarted = true
+                fragmentActivity.startActionMode(this)
+            }
+            setSelectionAndTitle(holder, favoriteEntity)
+            true
+        }
+
+        if (selectedItems.contains(favoriteEntity)) {
+            holder.setCardColors(R.color.colorPrimaryLighter, R.color.colorPrimary)
+        } else {
+            holder.setCardColors(R.color.white, R.color.lightMediumGray)
+        }
+    }
+
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        if (mode != null) {
+            actionMode = mode
+            mode.menuInflater.inflate(R.menu.favorites_contextual_menu, menu)
+        }
+        setStatusBarColor(R.color.darker)
+        return true
+    }
+
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.menu_delete_favorites -> {
+                for (favoriteEntity in selectedItems) mainViewModel.deleteFavorite(favoriteEntity)
+
+                Snackbar.make(
+                    rootView, "${selectedItems.size} Recipe/s Removed.", Snackbar.LENGTH_SHORT
+                ).setAction("Okay") {}.show()
+
+                selectedItems.clear()
+                isActionModeStarted = false
+                actionMode.finish()
+                true
+            }
+
+            else -> false
+        }
+    }
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
+        for (holder in viewHolders) holder.setCardColors(R.color.white, R.color.lightMediumGray)
+        selectedItems.clear()
+        isActionModeStarted = false
+        setStatusBarColor(R.color.colorPrimaryDark)
     }
 
     fun setFavorites(favoriteEntityList: List<FavoriteEntity>) {
@@ -37,6 +120,35 @@ class FavoritesAdapter : RecyclerView.Adapter<RecipesViewHolder>() {
         val diffUtilResult = DiffUtil.calculateDiff(diffUtilCallback)
         favorites = favoriteEntityList
         diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    fun finishActionMode() {
+        if (this::actionMode.isInitialized) actionMode.finish()
+    }
+
+    private fun setSelectionAndTitle(holder: RecipesViewHolder, favoriteEntity: FavoriteEntity) {
+        if (selectedItems.contains(favoriteEntity)) {
+            holder.setCardColors(R.color.white, R.color.lightMediumGray)
+            selectedItems.remove(favoriteEntity)
+        } else {
+            holder.setCardColors(R.color.colorPrimaryLighter, R.color.colorPrimary)
+            selectedItems.add(favoriteEntity)
+        }
+
+        when (selectedItems.size) {
+            0 -> {
+                actionMode.finish()
+                isActionModeStarted = false
+            }
+
+            1 -> actionMode.title = "${selectedItems.size} item selected"
+            else -> actionMode.title = "${selectedItems.size} items selected"
+        }
+    }
+
+    private fun setStatusBarColor(@ColorRes colorRes: Int) {
+        @Suppress("DEPRECATION")
+        fragmentActivity.window.statusBarColor = fragmentActivity.getColor(colorRes)
     }
 
 }
--- app/src/main/java/com/johnreg/recipeapp/utils/Functions.kt
+++ app/src/main/java/com/johnreg/recipeapp/utils/Functions.kt
@@ -4,10 +4,13 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Parcelable
 import android.widget.Toast
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import com.johnreg.recipeapp.ui.adapters.RecipesViewHolder
 
 fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
     observe(lifecycleOwner, object : Observer<T> {
@@ -25,4 +28,9 @@ inline fun <reified T : Parcelable> Bundle.getParcelableExtra(key: String): T? =
 
 fun Fragment.showToast(text: String) {
     Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+}
+
+fun RecipesViewHolder.setCardColors(@ColorRes backgroundColor: Int, @ColorRes strokeColor: Int) {
+    binding.cvRow.setBackgroundColor(ContextCompat.getColor(itemView.context, backgroundColor))
+    binding.cvRow.strokeColor = ContextCompat.getColor(itemView.context, strokeColor)
 }
