f7bb0d20b369bb42277c0222f150e8cc43fee56a
John Reg <johnreginaldjao@gmail.com>
2024-12-11 22:34:00 +0800

    Create favorites_menu

--- app/src/main/java/com/johnreg/recipeapp/ui/main/FavoritesFragment.kt
+++ app/src/main/java/com/johnreg/recipeapp/ui/main/FavoritesFragment.kt
@@ -2,11 +2,18 @@ package com.johnreg.recipeapp.ui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.johnreg.recipeapp.R
 import com.johnreg.recipeapp.databinding.FragmentFavoritesBinding
 import com.johnreg.recipeapp.ui.adapters.FavoritesAdapter
 import com.johnreg.recipeapp.viewmodels.MainViewModel
@@ -34,8 +41,33 @@ class FavoritesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         _favoritesAdapter = FavoritesAdapter(requireActivity(), mainViewModel)
 
+        setMenu()
         setRecyclerView()
         observeFavorites()
+    }
+
+    private fun setMenu() {
+        requireActivity().addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.favorites_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_delete_all -> {
+                        mainViewModel.deleteAllFavorites()
+
+                        Snackbar.make(
+                            requireView(), "All Recipes Removed.", Snackbar.LENGTH_SHORT
+                        ).setAction("Okay") {}.show()
+
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.STARTED)
     }
 
     override fun onDestroyView() {
