c7e262e0793c69bc100ebe717a9e94ab2d2c5913
John Reg <johnreginaldjao@gmail.com>
2025-01-15 22:08:55 +0800

    changed comment to kdoc

--- app/src/main/java/com/johnreg/recipeapp/RecipeApp.kt
+++ app/src/main/java/com/johnreg/recipeapp/RecipeApp.kt
@@ -3,6 +3,6 @@ package com.johnreg.recipeapp
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-// This is the place where the dagger components should be generated automatically for us
+/** This is the place where the dagger components should be generated automatically for us. */
 @HiltAndroidApp
 class RecipeApp : Application()
